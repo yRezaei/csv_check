f4c8586f55b727a70ddcc98f1fbc0657346364a6
yrezaei <yashar.a.rezaei@gmail.com>
2019-07-31 23:12:39 +0200

    minor improvements on interpolating first and last value

--- include/csv_utility.hpp
+++ include/csv_utility.hpp
@@ -11,114 +11,126 @@
 
 namespace csv
 {
-    namespace fs = std::filesystem;
+namespace fs = std::filesystem;
 
-    struct CSVData
+struct CSVData
+{
+    std::string header;
+    std::uint32_t item_per_raw;
+    std::vector<float> data;
+
+    friend std::ostream &operator<<(std::ostream &stream, const CSVData &csv_data)
     {
-        std::string header;
-        std::uint32_t item_per_raw;
-        std::vector<float> data;
-
-        friend std::ostream& operator<< (std::ostream& stream, const CSVData& csv_data)
+        if (!csv_data.header.empty())
+            stream << csv_data.header << '\n';
+        auto interval = csv_data.item_per_raw - 1;
+        for (auto i = 0u; i < csv_data.data.size(); i += csv_data.item_per_raw)
         {
-            if(!csv_data.header.empty())
-                stream << csv_data.header << '\n';
-            auto interval = csv_data.item_per_raw - 1;
-            for(auto i= 0u; i < csv_data.data.size(); i += csv_data.item_per_raw)
-            {
-                for(auto j = 0u; j < interval; ++j)
-                    stream << csv_data.data[i + j] << ", " ;
-                stream << csv_data.data[i + interval] << '\n';
-            }
-
-            return stream;
-        } 
-    };
-
-    inline void interpolate_bad_value(std::vector<float>& src, float bad_value = 0)
-    {
-        if(src.empty())
-            return;
-
-        float mu = 0.5f;
-
-        if(src.size() > 2)
-        {
-            for(auto itr = src.begin() + 1; itr != src.end() - 1; ++itr)
-            {
-                if(*itr == bad_value)
-                    *itr = math_util::interpolation::linear(*(itr-1), *(itr + 1), 0.5f);
-            }
+            for (auto j = 0u; j < interval; ++j)
+                stream << csv_data.data[i + j] << ", ";
+            stream << csv_data.data[i + interval] << '\n';
         }
 
-        if(src.size() > 1)
-            src.back() = math_util::interpolation::linear(*(src.end()-2), *(src.end()-1), 0.5f);
+        return stream;
+    }
+};
+
+inline void interpolate_bad_value(std::vector<float> &src, float bad_value = 0)
+{
+    if (src.empty())
+        return;
+
+    if (src.size() == 1)
+    {
+        if (src.front() == 0)
+            src.front() = 1;
+        return;
+    }
+    else
+    {
+        if (src.front() == 0)
+            src.front() = math_util::interpolation::linear(0, *(src.begin() + 1), 0.9f);
+
+        if (src.back() == 0)
+            src.back() = math_util::interpolation::linear(0, *(src.end() - 2), 1.1f);
+
+        if (src.size() > 2)
+        {
+            for (auto itr = src.begin() + 1; itr != src.end() - 1; ++itr)
+            {
+                if (*itr == bad_value)
+                    *itr = math_util::interpolation::linear(*(itr - 1), *(itr + 1), 0.5f);
+            }
+        }
+    }
+}
+
+inline void write_to_file(const std::string &file_path, CSVData &csv_data)
+{
+    std::ofstream file(file_path);
+    if (file.is_open())
+    {
+        file << csv_data;
+        file.close();
+    }
+}
+
+inline void read_from_file(const std::string &csv_file, CSVData &csv_data)
+{
+    auto file_path = fs::path(csv_file);
+    if (!fs::exists(file_path))
+    {
+        std::cerr << "ERROR in function 'csv::read_from_file' -> File not found: " << file_path << std::endl;
+        return;
     }
 
-    inline void write_to_file(const std::string& file_path, CSVData& csv_data)
+    if (!file_path.has_extension() || file_path.extension() != ".csv")
     {
-         std::ofstream file(file_path);
-        if(file.is_open())
-        {
-            file << csv_data;
-            file.close();
-        }
+        std::cerr << "ERROR in function 'csv::read_from_file' -> Unvalid file format: " << file_path << std::endl;
+        return;
     }
 
-    inline void read_from_file(const std::string& csv_file, CSVData& csv_data)
+    csv_data.data.clear();
+    csv_data.header = "";
+    csv_data.item_per_raw = 0;
+
+    bool has_header = false;
+    std::string line;
+    std::ifstream file(file_path);
+    if (file.is_open())
     {
-        auto file_path = fs::path(csv_file);
-        if(!fs::exists(file_path))
+        if (std::getline(file, line))
         {
-            std::cerr << "ERROR in function 'csv::read_from_file' -> File not found: " << file_path << std::endl;
-            return;
-        }
-
-        if(!file_path.has_extension() || file_path.extension() != ".csv")
-        {
-            std::cerr << "ERROR in function 'csv::read_from_file' -> Unvalid file format: " << file_path << std::endl;
-            return;
-        }
-
-        csv_data.data.clear();
-        csv_data.header = "";
-        csv_data.item_per_raw = 0;
-
-        bool has_header = false;
-        std::string line;
-        std::ifstream file(file_path);
-        if(file.is_open())
-        {
-            if(std::getline(file, line))
+            if (line.find_first_of("\"#") != std::string::npos)
             {
-                if(line.find_first_of("\"#") != std::string::npos) {
-                    csv_data.header = line;
-                    if(std::getline(file, line))
-                    {
-                        auto tokens = str_util::split(line, ",; ", str_util::TokenOption::REMOVE_EMPTY_TOKKEN);
-                        for(auto i = 0u; i < tokens.size(); ++i)
-                            csv_data.data.emplace_back(std::stof(tokens[i]));
-                        csv_data.item_per_raw = (std::uint16_t)tokens.size();
-                    }
-                }
-                else {
+                csv_data.header = line;
+                if (std::getline(file, line))
+                {
                     auto tokens = str_util::split(line, ",; ", str_util::TokenOption::REMOVE_EMPTY_TOKKEN);
-                    for(auto i = 0u; i < tokens.size(); ++i)
+                    for (auto i = 0u; i < tokens.size(); ++i)
                         csv_data.data.emplace_back(std::stof(tokens[i]));
                     csv_data.item_per_raw = (std::uint16_t)tokens.size();
                 }
             }
-
-            while(std::getline(file, line))
+            else
             {
-                auto tokens = str_util::split(line, ", ", str_util::TokenOption::REMOVE_EMPTY_TOKKEN);
-                for(auto i = 0u; i < tokens.size(); ++i)
+                auto tokens = str_util::split(line, ",; ", str_util::TokenOption::REMOVE_EMPTY_TOKKEN);
+                for (auto i = 0u; i < tokens.size(); ++i)
                     csv_data.data.emplace_back(std::stof(tokens[i]));
+                csv_data.item_per_raw = (std::uint16_t)tokens.size();
             }
-
-            file.close();
         }
+
+        while (std::getline(file, line))
+        {
+            auto tokens = str_util::split(line, ", ", str_util::TokenOption::REMOVE_EMPTY_TOKKEN);
+            for (auto i = 0u; i < tokens.size(); ++i)
+                csv_data.data.emplace_back(std::stof(tokens[i]));
+        }
+
+        file.close();
     }
 }
+} // namespace csv
 
 #endif // !CSV_UTILITY_HPP_
